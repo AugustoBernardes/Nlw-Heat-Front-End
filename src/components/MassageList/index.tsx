8f02ae4700bb262567737aa877cd31eeddda2f6e
Augusto <augustobdev@outlook.com>
2022-03-18 11:01:19 -0300

    feature - CSS done

--- src/components/MassageList/index.tsx
+++ src/components/MassageList/index.tsx
@@ -1,13 +1,33 @@
 import styles from "./styles.module.scss"
 import logoImg from "../../assets/logo.svg"
+import { useEffect, useState } from "react"
+import { api } from "../../services/api"
+
+// Showing the message type to use
+type Message = {
+    id:string;
+    text:string;
+    user:{
+        name:string;
+        avatar_url:string;
+    }
+}
 
 export function MessageList(){
+    const [messages, setMessages] = useState<Message[]>([])
+
+    useEffect(() => {
+        api.get<Message[]>('messages/last3').then(response => {
+            setMessages(response.data)
+        })
+    }, [])
 
     return (
         <div className={styles.messageListWrapper}>
             <img src={logoImg} alt="DoWhile2021" />
 
             <ul className={styles.messageList}> 
+                
                 <li className={styles.message}>
                     <p className={styles.messageContent}>Essa é uma mensagemEssa é uma mensagemEssa é uma mensagemEssa é uma mensagemEssa é uma mensagemEssa é uma mensagemEssa é uma mensagemEssa é uma mensagemEssa é uma mensagem</p>
                     <div className={styles.messageUser}>
@@ -18,25 +38,7 @@ export function MessageList(){
                     </div>
                 </li>
 
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Essa é uma mensagemEssa é uma mensagemEssa é uma mensagemEssa é uma mensagemEssa é uma mensagemEssa é uma mensagemEssa é uma mensagemEssa é uma mensagemEssa é uma mensagem</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/AugustoBernardes.png" alt="Augusto Bernardes" />
-                        </div>
-                        <span>Diego Fernandes</span>
-                    </div>
-                </li>
-
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Essa é uma mensagemEssa é uma mensagemEssa é uma mensagemEssa é uma mensagemEssa é uma mensagemEssa é uma mensagemEssa é uma mensagemEssa é uma mensagemEssa é uma mensagem</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/AugustoBernardes.png" alt="Augusto Bernardes" />
-                        </div>
-                        <span>Diego Fernandes</span>
-                    </div>
-                </li>
+               
             </ul>
         </div>
     )
