4c980316922c6aefc5252db38e90825a221301da
Augusto <augustobdev@outlook.com>
2022-03-21 10:20:27 -0300

    feature - Login with GitHub and separe contexts

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,49 +1,14 @@
 import { VscGithubInverted } from "react-icons/vsc"
 import styles from "./styles.module.scss";
-import { api } from "../../services/api"
-import { useEffect } from "react";
+import { AuthContext } from "../../contexts/auth"
+import { useContext } from "react";
 
-type AuthResponse = {
-    token:string;
-    user:{
-        id:string;
-        avatar_url:string;
-        name:string;
-        login:string
-    }
-}
 
 
 export function LoginBox(){
-    const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=5292af6a016c981023c6`
+    const { signInUrl,user } = useContext(AuthContext);
 
-    async function signIn(githubCode:string){
-        const response = await api.post<AuthResponse>('authenticate',{
-            code:githubCode,
-        })
-
-        const { token,user } = response.data
-
-        localStorage.setItem('@dowhile:token',token)
-
-        console.log(user)
-    }
-
-    useEffect(() =>{
-        const url = window.location.href;
-
-        const hasGitHubCode = url.includes('?code=')
-
-        if(hasGitHubCode){
-            // Split the URL with two data 
-            const [urlWithoutCode, githubCode] = url.split('?code=')
-
-            // Removing GitHub Code from URL
-            window.history.pushState({},'',urlWithoutCode)
-
-            signIn(githubCode)
-        }
-    },[])
+    console.log(user)
 
     return (
         <div className={styles.loginBoxWrapper}>
---
+++ src/contexts/auth.tsx
@@ -0,0 +1,92 @@
+import { createContext, ReactNode } from "react"
+import { api } from "../services/api"
+import { useEffect,useState } from "react";
+
+
+// Setting Types
+type User = {
+    id:string;
+    name:string;
+    login:string;
+    avatar_url:string
+}
+
+type AuthContextData ={
+    user:User | null;
+    signInUrl:string;
+    signOut:() => void;
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+type AuthProvider = {
+    children:ReactNode;
+}
+
+
+type AuthResponse = {
+    token:string;
+    user:{
+        id:string;
+        avatar_url:string;
+        name:string;
+        login:string
+    }
+}
+
+export function AuthProvider(props:AuthProvider){
+    const [user, setUser] = useState<User | null>(null);
+
+    const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=5292af6a016c981023c6`
+
+    function signOut(){
+        setUser(null);
+        localStorage.removeItem('@dowhile:token')
+    }
+
+    async function signIn(githubCode:string){
+        const response = await api.post<AuthResponse>('authenticate',{
+            code:githubCode,
+        })
+
+        const { token,user } = response.data
+
+        localStorage.setItem('@dowhile:token',token)
+
+        setUser(user)
+    }
+
+    useEffect(() =>{
+        const token = localStorage.getItem('@dowhile:token')
+        if(token){
+
+            api.defaults.headers.common.authorization = `Baerer ${token}`
+
+            api.get<User>('profile').then(response => {
+                setUser(response.data)
+            })
+        }
+    },[])
+
+    useEffect(() =>{
+        const url = window.location.href;
+
+        const hasGitHubCode = url.includes('?code=')
+
+        if(hasGitHubCode){
+            // Split the URL with two data 
+            const [urlWithoutCode, githubCode] = url.split('?code=')
+
+            // Removing GitHub Code from URL
+            window.history.pushState({},'',urlWithoutCode)
+
+            signIn(githubCode)
+        }
+    },[])
+
+    return( 
+        <AuthContext.Provider value={{signInUrl,user,signOut}}>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
